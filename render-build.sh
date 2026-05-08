55ce76f485957f96bf12974add6a2da1fa4492e5
Hemithea <seydanuraydin50@gmail.com>
2026-05-08 13:03:09 +0300

    Remove Turkish and NER model download command
    Removed the command to download the Turkish and NER model to save RAM.

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,3 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Sadece gerekli olan Türkçe ve NER modelini indiriyoruz (RAM tasarrufu)
-python -c "import stanza; stanza.download('tr', processors='tokenize,ner')"
