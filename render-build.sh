cf5654bec703326d5ba4cd31e5394c10bd4427ad
Hemithea <seydanuraydin50@gmail.com>
2026-05-08 08:40:22 +0300

    Revise dependency installation and model download
    Updated comments and changed model installation method.

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Bağımlılıkları kur
+# 1. Bağımlılıkları kur
 pip install -r requirements.txt
 
-# Türkçe modelini güvenli yoldan indir
-#python -m spacy download tr_core_news_lg
+# 2. Türkçe modelini doğrudan pip üzerinden GitHub reposundan çek (Linkten daha güvenli)
+pip install https://github.com/explosion/spacy-models/releases/download/tr_core_news_lg-3.7.0/tr_core_news_lg-3.7.0-py3-none-any.whl
