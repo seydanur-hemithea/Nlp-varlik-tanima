a637dc9b409349e6eac369aaff67fe158564dda2
Hemithea <seydanuraydin50@gmail.com>
2026-05-08 11:19:38 +0300

    Clean up comments in render-build.sh
    Remove comments about installing basic packages and downloading the Medium model.

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Temel paketleri kur
+# Sadece requirements'daki kütüphaneleri kur
 pip install -r requirements.txt
-
-# 2. Medium modelini güvenli kaynaktan indir
-python -m spacy download tr_core_news_md
